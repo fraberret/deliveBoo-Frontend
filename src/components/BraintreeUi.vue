84111394e26c32c3cf52f2d3b9141f930e7598e3
Nico-Serra <nico.serra000@gmail.com>
2024-06-22 14:51:34 +0200

    add modal in brainTreeUi

--- src/components/BraintreeUi.vue
+++ src/components/BraintreeUi.vue
@@ -31,8 +31,32 @@ export default {
 </script>
 
 <template>
-    <div id="dropin-container"></div>
-    <button id="submit-button" class="button button--small button--green">Purchase</button>
+    <!-- Modal trigger button -->
+    <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#modalId">
+        Buy
+    </button>
+
+    <!-- Modal Body -->
+    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+    <div class="modal fade" id="modalId" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+        aria-labelledby="modalTitleId" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitleId">
+                        Choise your method of pay
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div id="dropin-container"></div>
+                    <button id="submit-button" class="button button--small button--green">Purchase</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </template>
 
 <style>
