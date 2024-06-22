746452aa7bc3ceed1d3dfcf661cb5ce0b56aa6f9
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-22 13:43:15 +0200

    fix header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,45 +1,125 @@
 <script>
+import { store } from '../store';
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    data() {
+        return {
+            store
+        }
+    },
+    mounted() {
+        store.localCart.forEach(item => {
+            store.cartQuantity += item.quantity
+        });
+        this.cartQuantity = store.localCart.reduce((sum, item) => sum + item.quantity, 0);
+    },
 }
 </script>
 
 
 <template>
     <header>
-        <nav class="navbar navbar-expand-md navbar-light py-4 mb-5">
-            <div class="container">
-                <router-link :to="{ name: 'home' }" class="navbar-brand d-flex align-items-center" href="">
-                    <div class="logo_laravel ps-2">
-                        <img src="/img/logo-deliveboo.png" alt="app logo">
+        <nav class="container">
+            <router-link class="logo" :to="{ name: 'home' }">
+                <img src="/img/logo-deliveboo.png" alt="app logo">
+            </router-link>
+            <div class="nav_links">
+
+            </div>
+            <div class="actions">
+                <router-link class="cart" :to="{ name: 'cart' }">
+                    <img src="/img/cart-icon.png" alt="cart icon">
+                    <div v-if="store.cartQuantity > 0" class="cartCounter">
+                        <small>{{ store.cartQuantity }}</small>
                     </div>
                 </router-link>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                    <ul class="navbar-nav ms-auto">
-
-                        <li class="nav-item">
-                            <router-link class="nav-link nav_link px-4" :to="{ name: 'cart' }">Cart</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link nav_link px-4" href="http://localhost:8000/login">I own a place</a>
-                        </li>
-
-
-                    </ul>
-                </div>
+                <a class="buttons btn_primary" href="http://localhost:8000/login">I own a place</a>
             </div>
         </nav>
     </header>
+
 </template>
 
 
 
-<style></style>
+<style>
+header {
+    margin-bottom: 3rem;
+
+    nav {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 1.25rem 0;
+
+        .nav_links {
+            display: flex;
+            align-items: center;
+            gap: 3rem;
+
+            .nav_link {
+                color: var(--boo-gray-700);
+                text-decoration: none;
+                font-weight: 500;
+                letter-spacing: .5px;
+                font-size: .9rem;
+                position: relative;
+                display: flex;
+                justify-content: center;
+
+                &:hover {
+                    color: var(--boo-darker);
+                }
+
+                &::after {
+                    content: '';
+                    top: 2rem;
+                    position: absolute;
+                    width: 4px;
+                    aspect-ratio: 1;
+                    border-radius: 50%;
+                    background-color: transparent;
+                    transition: background-color .2s ease;
+                }
+
+                &:hover::after {
+                    background-color: var(--boo-gray-600);
+                }
+            }
+        }
+
+        .actions {
+            display: flex;
+            align-items: center;
+            gap: 2rem;
+
+            .cart {
+                position: relative;
+
+                .cartCounter {
+                    position: absolute;
+                    width: 21px;
+                    height: 21px;
+                    border-radius: 50%;
+                    top: 13px;
+                    left: 10px;
+                    font-size: 13px;
+                    background-color: var(--boo-primary);
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    color: var(--boo-lighter);
+                }
+            }
+
+            .btn_primary {
+                font-size: 0.9rem;
+                height: 3rem;
+                font-weight: 300;
+                padding-inline: 1.6rem;
+                text-decoration: none;
+            }
+        }
+    }
+}
+</style>
