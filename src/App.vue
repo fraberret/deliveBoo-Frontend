dae9a2b7af332de7a1902ddf2107493b1b0c497e
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-25 18:33:30 +0200

    add page loader only on the homepage

--- src/App.vue
+++ src/App.vue
@@ -5,42 +5,39 @@ import AppFooter from './components/AppFooter.vue';
 import PageLoader from './components/PageLoader.vue';
 
 export default {
-  name: 'App',
-  components: {
-    AppHeader,
-    AppFooter,
-    PageLoader
-  },
-  data() {
-    return {
-      showLoader: false
-    };
-  },
-  beforeRouteEnter(to, from, next) {
-    if (to.path === '/') {
-      next(vm => {
-        vm.showLoader = true;
-      });
-    } else {
-      next(vm => {
-        vm.showLoader = false;
-      });
-    }
-  }
-
+    name: 'App',
+    components: {
+        AppHeader,
+        AppFooter,
+        PageLoader
+    },
+    data() {
+        return {
+            showLoader: false
+        };
+    },
+    mounted() {
+        this.$watch(() => this.$route, (to) => {
+            if (to.name === 'home') {
+                this.showLoader = true;
+            } else {
+                this.showLoader = false;
+            }
+        });
+    },
 }
 </script>
 
 <template>
-  <PageLoader v-if="showLoader"></PageLoader>
+    <PageLoader v-if="showLoader"></PageLoader>
 
-  <AppHeader></AppHeader>
+    <AppHeader></AppHeader>
 
-  <main>
-    <RouterView />
-  </main>
+    <main>
+        <RouterView />
+    </main>
 
-  <AppFooter></AppFooter>
+    <AppFooter></AppFooter>
 </template>
 
 <style></style>
